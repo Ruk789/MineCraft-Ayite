ee62a1b36aecd0f779ad726b70ed720b2929eaf4
Ruk789 <94543812+Ruk789@users.noreply.github.com>
2023-08-20 21:16:57 +0800

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,29 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: '简单易用',
+    title: '帮助须知',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        就这点配置有啥难的.
+        保持良好的素质是我们的基本原则
       </>
     ),
   },
   {
-    title: '文档详细',
+    title: '资源共享',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        看完文档学不会怎么用说明你是个傻逼.
+        服务器编写的插件可公益使用 　　( xxxx )
       </>
     ),
   },
   {
-    title: '功能强大',
+    title: '无',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        多说无益, 用用就知道了.
+        
       </>
     ),
   },
